a6243d43aca5330d4070aa0ec8f6ebc433e4a3f4
abhipsamishra032 <30312391+abhipsamishra032@users.noreply.github.com>
2018-03-17 05:23:17 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a root element
+export default class App extends Component {
+    render() {
+        return(
+            <div className="root">React-webpack application</div>
+        );
+    }
+}
